6efc417a70ff7cd3b668deaf035bb8f250a31360
MrDrich <inigoaldrich24@gmail.com>
2025-03-21 23:42:21 +0800

    Update AgentInfo by adding new columns specifically agentCode

--- backend/models/agentInfo.model.js
+++ backend/models/agentInfo.model.js
@@ -2,6 +2,12 @@ import mongoose from "mongoose";
 
 const agentInfoSchema = mongoose.Schema(
   {
+    agentCode: {
+      type: String,
+      required: [true, "Agent code is required"],
+      maxlength: [50, "Agent code cannot exceed 50 characters"],
+      trim: true,
+    },
     tin: {
       type: String,
       required: [true, "TIN is required"],
@@ -20,6 +26,16 @@ const agentInfoSchema = mongoose.Schema(
       maxlength: [255, "Registered name cannot exceed 255 characters"],
       trim: true,
     },
+    agentName: {
+      type: String,
+      required: [true, "Agent name is required"],
+      trim: true,
+    },
+    tradeName: {
+      type: String,
+      required: [true, "Trade name is required"],
+      trim: true,
+    },
     agentType: {
       type: String,
       required: [true, "Agent type is required"],
@@ -34,21 +50,18 @@ const agentInfoSchema = mongoose.Schema(
     },
     authorizedRepresentative: {
       type: String,
-      required: [true, "Authorized representative is required"],
-      maxlength: [
-        255,
-        "Authorized representative cannot exceed 255 characters",
-      ],
+      // required: [true, "Authorized representative is required"],
+      // maxlength: [255,"Authorized representative cannot exceed 255 characters",],
       trim: true,
     },
-    address: {
+    agentAddress: {
       type: String,
       required: [true, "Address is required"],
       trim: true,
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
+      // required: [true, "Email is required"],
       trim: true,
       match: [
         /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
@@ -57,14 +70,18 @@ const agentInfoSchema = mongoose.Schema(
     },
     phone: {
       type: String,
-      required: [true, "Phone is required"],
-      maxlength: [50, "Phone cannot exceed 50 characters"],
+      // required: [true, "Phone is required"],
+      // maxlength: [50, "Phone cannot exceed 50 characters"],
       trim: true,
     },
     fax: {
       type: String,
-      required: [true, "Fax is required"],
-      maxlength: [50, "Fax cannot exceed 50 characters"],
+      // required: [true, "Fax is required"],
+      // maxlength: [50, "Fax cannot exceed 50 characters"],
+      trim: true,
+    },
+    website: {
+      type: String,
       trim: true,
     },
     isDeleted: {

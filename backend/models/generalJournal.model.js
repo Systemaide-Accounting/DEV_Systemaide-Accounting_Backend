482a43f1d6279957d62f3a1df29e3e465288b744
AaronJay30 <aaronjaygabato30@gmail.com>
2025-05-01 15:52:08 +0800

    Refactor transaction models to include totalDebit and totalCredit fields, and adjust required validations for month and year.

--- backend/models/generalJournal.model.js
+++ backend/models/generalJournal.model.js
@@ -1,55 +1,65 @@
 import mongoose from "mongoose";
 
 const generalJournalSchema = mongoose.Schema(
-	{
-		date: {
-			type: Date,
-			required: [true, "Date is required"],
-			trim: true,
-		},
-		month: {
-			type: String,
-			// required: [true, "Month is required"],
-			trim: true,
-		},
-		year: {
-			type: String,
-			// required: [true, "Year is required"],
-			trim: true,
-		},
-		location: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Location",
-			// required: [true, "Location is required"],
-			trim: true,
-		},
-		jvNo: {
-			type: String,
-			required: [true, "JV No is required"],
-			maxlength: [50, "JV No cannot exceed 50 characters"],
-			trim: true,
-		},
-		particular: {
-			type: String,
-			required: [true, "Particular is required"],
-			// maxlength: [255, "Particular cannot exceed 255 characters"],
-			trim: true,
-		},
-		transactionLines: {
-			type: String,
-			trim: true,
-		},
-		isDeleted: {
-			type: Boolean,
-			default: false,
-		},
-		deletedAt: {
-			type: Date,
-		},
-	},
-	{
-		timestamps: true,
-	}
+    {
+        date: {
+            type: Date,
+            required: [true, "Date is required"],
+            trim: true,
+        },
+        month: {
+            type: String,
+            // required: [true, "Month is required"],
+            trim: true,
+        },
+        year: {
+            type: String,
+            // required: [true, "Year is required"],
+            trim: true,
+        },
+        location: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Location",
+            // required: [true, "Location is required"],
+            trim: true,
+        },
+        jvNo: {
+            type: String,
+            required: [true, "JV No is required"],
+            maxlength: [50, "JV No cannot exceed 50 characters"],
+            trim: true,
+        },
+        particular: {
+            type: String,
+            required: [true, "Particular is required"],
+            // maxlength: [255, "Particular cannot exceed 255 characters"],
+            trim: true,
+        },
+        totalDebit: {
+            type: String,
+            // required: [true, "Total Debit is required"],
+            trim: true,
+        },
+        totalCredit: {
+            type: String,
+            // required: [true, "Total Credit is required"],
+            trim: true,
+        },
+        transactionLines: {
+            type: String,
+            trim: true,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: {
+            type: Date,
+        },
+    },
+    {
+        timestamps: true,
+    }
 );
 
 const GeneralJournal = mongoose.model("GeneralJournal", generalJournalSchema);
--- backend/models/purchaseOnAccountTransaction.model.js
+++ backend/models/purchaseOnAccountTransaction.model.js
@@ -1,88 +1,95 @@
 import mongoose from "mongoose";
 
 const purchaseOnAccountTransactionSchema = mongoose.Schema(
-  {
-    date: {
-      type: Date,
-      required: [true, "Date is required"],
-      default: Date.now,
-    },
-    month: {
-      type: String,
-      // required: [true, "Month is required"],
-      trim: true,
-    },
-    year: {
-      type: String,
-      // required: [true, "Year is required"],
-      trim: true,
-    },
-    location: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Location",
-      required: [true, "Location is required"],
-    },
-    pvNo: {
-      type: String,
-      required: [true, "PV No is required"],
-      maxlength: [50, "PV No cannot exceed 50 characters"],
-      trim: true,
-    },
-    invoiceNo: {
-      type: String,
-      required: [true, "Invoice No is required"],
-      maxlength: [50, "Invoice No cannot exceed 50 characters"],
-      trim: true,
-    },
-    supplierName: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "AgentInfo",
-      required: [true, "Supplier Name is required"],
-    },
-    address: {
-      // value based on supplier name or agent
-      type: String,
-      maxlength: [255, "Address cannot exceed 255 characters"],
-      trim: true,
-    },
-    tin: {
-      // value based on supplier name or agent
-      type: String,
-      maxlength: [255, "TIN cannot exceed 255 characters"],
-      trim: true,
-    },
-    particular: {
-      type: String,
-      required: [true, "Particular is required"],
-      // maxlength: [255, "Particular cannot exceed 255 characters"],
-      trim: true,
-    },
+    {
+        date: {
+            type: Date,
+            required: [true, "Date is required"],
+            default: Date.now,
+        },
+        month: {
+            type: String,
+            // required: [true, "Month is required"],
+            trim: true,
+        },
+        year: {
+            type: String,
+            // required: [true, "Year is required"],
+            trim: true,
+        },
+        location: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Location",
+            required: [true, "Location is required"],
+        },
+        pvNo: {
+            type: String,
+            required: [true, "PV No is required"],
+            maxlength: [50, "PV No cannot exceed 50 characters"],
+            trim: true,
+        },
+        invoiceNo: {
+            type: String,
+            required: [true, "Invoice No is required"],
+            maxlength: [50, "Invoice No cannot exceed 50 characters"],
+            trim: true,
+        },
+        supplierName: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "AgentInfo",
+            required: [true, "Supplier Name is required"],
+        },
+        address: {
+            // value based on supplier name or agent
+            type: String,
+            maxlength: [255, "Address cannot exceed 255 characters"],
+            trim: true,
+        },
+        tin: {
+            // value based on supplier name or agent
+            type: String,
+            maxlength: [255, "TIN cannot exceed 255 characters"],
+            trim: true,
+        },
+        particular: {
+            type: String,
+            required: [true, "Particular is required"],
+            // maxlength: [255, "Particular cannot exceed 255 characters"],
+            trim: true,
+        },
+        totalDebit: {
+            type: String,
+            // required: [true, "Total Debit is required"],
+            trim: true,
+        },
+        totalCredit: {
+            type: String,
+            // required: [true, "Total Credit is required"],
+            trim: true,
+        },
+        // B. Transaction Template
+        transactionLines: {
+            type: String,
+            trim: true,
+        },
 
-    // B. Transaction Template
-		transactionLines: {
-			type: String,
-			trim: true,
-		},
-
-    // System/Soft Delete Fields
-    isDeleted: {
-      type: Boolean,
-      default: false,
+        // System/Soft Delete Fields
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: {
+            type: Date,
+        },
     },
-    deletedAt: {
-      type: Date,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 const PurchaseOnAccountTransaction = mongoose.model(
-  "PurchaseOnAccountTransaction",
-  purchaseOnAccountTransactionSchema
+    "PurchaseOnAccountTransaction",
+    purchaseOnAccountTransactionSchema
 );
-
-
 
 export default PurchaseOnAccountTransaction;
